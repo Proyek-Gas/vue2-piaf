6977d4330969bb5cf91c84162030c14cfa416273
Fuzors-f <54881946+Fuzors-f@users.noreply.github.com>
2021-08-11 16:49:44 +0700

    detailSellingPrice

---
+++ src/views/app/item/page/tableSellingPrice.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="row">
+      <div class="col-12">
+
+           <vuetable
+              ref="vuetable"
+              style="display:block; overflow-x:auto;width:auto"
+              :api-mode="false"
+              :fields="fields"
+              :data="dataComponent"
+              >
+              <template slot="price" slot-scope="props">
+                {{props.rowData.price | currency}}
+              </template>
+           </vuetable>
+      </div>
+    </div>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+import Vuetable from "vuetable-2/src/components/Vuetable";
+import VuetablePaginationBootstrap from "../../../../components/Common/VuetablePaginationBootstrap";
+import DatatableHeading from "../../../../containers/datatable/DatatableHeading";
+
+export default{
+ props : ['dataComponent'],
+  computed : {
+     ...mapGetters({ currentUser : "currentuser"})
+  },
+  components : {
+    vuetable: Vuetable,
+
+  },
+
+  data(){
+    return {
+
+        fields : [
+          {
+             name : "priceCategory.id",
+              title : "id",
+              titleClass: "",
+               sortField: "priceCategory.id",
+              dataClass: "text-muted",
+              width : "50%"
+          },
+          {
+            name : "priceCategory.name",
+            title : "Name",
+            titleClass : "",
+             sortField: "priceCategory.name",
+            dataClass : "text-muted",
+            width : "40%"
+          },
+            {
+            name : "__slot :price",
+            title : "Price",
+            titleClass : "",
+             sortField: "price",
+            dataClass : "text-muted",
+            width : "60%"
+          }
+        ],
+
+    }
+  }
+}
+</script>
